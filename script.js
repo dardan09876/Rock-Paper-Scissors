5f1a9e2091a753ad499f8b8c9e3b4826458455e8
dardan09876 <dardan09876@gmail.com>
2025-02-14 16:57:49 -0800

    Function to get the computers choice

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    const choice = Math.random();
+
+    if (choice < 1 / 3) {
+        return "rock";
+    } else if (choice < 2 / 3) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
